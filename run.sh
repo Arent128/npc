acccd3dbb3ad1c95fbf4bcc2e75005b6d0f1d1b3
B-Tran <btran054@ucr.edu>
2020-03-12 20:14:59 -0700

    Added script to run tests for parse.py
    - Script command contain:
        running tests
        running test coverage
        running mutex coverage with html report
        running mutex coverage with detailed results report

---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Script for running the tests
+
+if [[ "$1" == "-h" ]];
+then
+    echo "Usage:"
+    echo "-r    Runs the test for parser"
+    echo "-c    Runs test coverage for parser"
+    echo "-rh   Runs mutex coverage for parser and makes a html report"
+    echo "-rm   Runs the mutex report for parser and makes a text report"
+elif [[ "$1" == "-r" ]];
+then
+    pytest tests/test_parser.py
+#-c runs the coverage for test_parser
+elif [[ "$1" == "-c" ]]; 
+then
+    if [ -d "htmlcov" ];
+    then
+        rm -rf htmlcov
+    fi
+    python -m pytest --cov=test_parser
+    coverage html tests/test_parser.py
+#runs the mutex tests for test_parser and makes an html report
+elif [[ "$1" == "-rh" ]];
+then
+    mut.py --target npc/parser.py --unit-test tests/test_parser.py --report-html mutpy_report/
+#runs the mutex tests for test_parser and returns the results to a text
+#file in mutpy_report as result.txt
+elif [[ "$1" == "-rm" ]];
+then
+    echo "Writing report..."
+    mut.py --target npc/parser.py --unit-test tests/test_parser.py -m > "mutpy_report/result.txt"
+    echo "Report complete."
+else
+    echo "Command not found."
+    echo "use -h for help."
+fi
